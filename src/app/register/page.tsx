2b8d3eb692e57410a6b9799d2027eebd9a815980
kabir shuvo <kabirshuvo19@gmail.com>
2024-02-02 03:45:52 +0600

    Folder structure updated

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,15 +1,16 @@
-import Image from "next/image";
+import React from "react";
 
-export default function Home() {
+const RegisterPage = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        Home Page
+        Register Page
       </div>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-       Next.js 
+        Next.js
       </div>
     </main>
   );
-}
+};
+
+export default RegisterPage;
---
+++ src/app/services/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ServicesPage = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+    <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      Services Page
+    </div>
+    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+     Next.js 
+    </div>
+  </main>
+  )
+}
+
+export default ServicesPage
