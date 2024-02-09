fef4ad4f0d4a6a59510cdba6b7264e8d869f4336
Kabir <kabirshuvo19@gmail.com>
2024-02-09 14:33:12 -0800

    Logos added to the public folder and updated skills Page

---
+++ src/constants/index.ts
@@ -0,0 +1,137 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Adobe Photoshop",
+    Image: "/Adobe_Photoshop_logo.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Adobe Illustrator",
+    Image: "/AdobeIllustrator_logo.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Adobe In-Design",
+    Image: "/Adobe_InDesign_logo.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "TypoGraphy",
+    Image: "/Font_Logo.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Mokups",
+    Image: "/preepress_logo.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "PreePress Software",
+    Image: "/preepress-text_logo.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    name: "Color Management",
+    Image: "/cmyk_plus_logo.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "DTG - Solutions",
+    Image: "/CMYK_Search_Logo.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Sublimation Printing",
+    Image: "/preepress_logo.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Mid Journey",
+    Image: "/Midjourney.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "FontForge",
+    Image: "/Font_forge_logo.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Google Fonts",
+    Image: "/Google_fonts_logo.png",
+    width: 40,
+    height: 40,
+  },
+];
+
+export const Socials = [
+  {
+    name: "Discord",
+    src: "/instagram.svg",
+  },
+  {
+    name: "Facebook",
+    src: "/facebook.svg",
+  },
+  {
+    name: "Instagram",
+    src: "/discord.svg",
+  },
+];
+export const Projects = [
+  {
+    title: "A Decade of Innovation and Creativity",
+    text: "Explore a decade-long journey of innovation and creativity, showcasing a diverse range of design projects spanning textile engineering and graphic design domains. From conceptualization to execution, each project reflects a unique blend of technical expertise and artistic flair, pushing the boundaries of traditional design practices.",
+    src: "/Creativity.png",
+  },
+  {
+    title: "Diving Deep into Textile Engineering Excellence",
+    text: "Delve into the realm of textile engineering excellence, where precision meets creativity. Discover how cutting-edge technologies and meticulous craftsmanship converge to produce textiles that not only meet but exceed industry standards. From fiber to fabric, each project is a testament to the relentless pursuit of quality and innovation.",
+    src: "/Excellence.png",
+  },
+  {
+    title: "Innovative Solutions, Creative Designs",
+    text: "Experience the intersection of innovation and creativity, where solutions are born out of imagination and ingenuity. From solving complex technical challenges to crafting visually captivating designs, each project embodies a spirit of innovation and a commitment to pushing the boundaries of what's possible.",
+    src: "/Solutions.png",
+  },
+  {
+    title: "Textile Innovations and Design Inspirations",
+    text: "Uncover a treasure trove of textile innovations and design inspirations that ignite imagination and spark creativity. From groundbreaking materials to visionary design concepts, each project is a source of inspiration, driving forward the evolution of textile engineering and graphic design alike.",
+    src: "/Inspirations.png",
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "/contact-me",
+    icon: RxClipboard,
+    link: "/contact-me",
+  },
+];
