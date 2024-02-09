ff1d5e8e1255a48e40b0b7e2a510a8b09754ec19
Kabir <kabirshuvo19@gmail.com>
2024-02-09 10:40:46 -0800

    Design page updated

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,17 +1,11 @@
-import React from 'react'
+import React from "react";
+import ContactForm from "../components/ContactForm";
+import ProjectCard from "../components/ProjectCard";
+import Link from "next/link";
+import Image from "next/image";
 
 const AboutPage = () => {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  return <>About Page Comming Soon</>;
+};
 
-    <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-      About Page
-    </div>
-    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-     Next.js 
-    </div>
-  </main>
-  )
-}
-
-export default AboutPage
+export default AboutPage;
