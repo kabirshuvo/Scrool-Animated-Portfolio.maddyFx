61264e216027797b0abfe8fbba726b2e199a23e7
Kabir <kabirshuvo19@gmail.com>
2024-02-10 11:52:29 +0600

    Navbar updated

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,32 +1,40 @@
 import Link from "next/link";
 import React from "react";
 import ThemeSwitcher from "./ThemeSwitcher";
+import Image from "next/image";
 
 const NavBar = () => {
   return (
-    <nav className="container flex items-center justify-between absolute top-0 z-10">
-      <ul className="flex gap-7 pt-2">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/designs">Designs</Link>
-        </li>
-        <li>
-          <Link href="/offers">Offers</Link>
-        </li>
-        <li>
-          <Link href="/services">Services</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
-      </ul>
-      <ThemeSwitcher />
-    </nav>
+    <div className="flex justify-center items-center">
+      <nav className="w-full flex items-center justify-between absolute top-0 z-10 py-2 border-b border-slate-700 ">
+        <div>
+          <Image src="/cmyk_plus_logo.png" alt="Logo" width={30} height={30} />
+        </div>
+        <div>
+          <ul className="flex gap-7 pt-2">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/designs">Designs</Link>
+            </li>
+            <li>
+              <Link href="/offers">Offers</Link>
+            </li>
+            <li>
+              <Link href="/services">Services</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+        <ThemeSwitcher />
+      </nav>
+    </div>
   );
 };
 
