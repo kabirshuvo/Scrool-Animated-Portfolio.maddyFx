cd88d4ccbc3efd0d7e54834198eddfdf29058c61
Kabir <kabirshuvo19@gmail.com>
2024-02-10 09:20:37 +0600

    Navbar updated to absolute top 0 position and z-10

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,34 +1,33 @@
-import Link from 'next/link'
-import React from 'react'
-import ThemeSwitcher from './ThemeSwitcher'
+import Link from "next/link";
+import React from "react";
+import ThemeSwitcher from "./ThemeSwitcher";
 
 const NavBar = () => {
   return (
-    <nav className='container flex items-center justify-between'>
-              <ul className='flex gap-7'>
-                <li>
-                  <Link href='/'>Home</Link>
-                </li>
-                <li>
-                  <Link href='/about'>About</Link>
-                </li>
-                <li>
-                  <Link href='/designs'>Designs</Link>
-                </li>
-                <li>
-                  <Link href='/offers'>Offers</Link>
-                </li>
-                <li>
-                  <Link href='/services'>Services</Link>
-                </li>
-                <li>
-                    <Link href='/contact'>Contact</Link>
-                </li>
-               
-              </ul>
-              <ThemeSwitcher />
-            </nav>
-  )
-}
+    <nav className="container flex items-center justify-between absolute top-0 z-10">
+      <ul className="flex gap-7 pt-2">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/designs">Designs</Link>
+        </li>
+        <li>
+          <Link href="/offers">Offers</Link>
+        </li>
+        <li>
+          <Link href="/services">Services</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+      <ThemeSwitcher />
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
