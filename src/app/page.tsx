bb57caafc1f8a6d43c8b824a5b6da57c399c00f6
Kabir <kabirshuvo19@gmail.com>
2024-02-15 12:32:17 +0600

    Modal added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,6 @@ export default function Home() {
 
       <MyHobbies />
       <TextAnimation value={Paragraph} />
-      {/* <Footer /> */}
     </main>
   );
 }
--- src/components/HeroSection2D.tsx
+++ src/components/HeroSection2D.tsx
@@ -58,11 +58,11 @@ const HeroSection2D = () => {
       className="flex flex-col md:flex-row min-h-screen"
       style={{ perspective: "1000px" }}
     >
-      <div className="w-full md:w-1/2 text-center md:text-right flex flex-col justify-center items-center p-8">
-        <div className="text-5xl md:text-7xl my-7 text-[#f2994a]">
-          <h1 className="flex gap-11">I came, I saw, I won</h1>
+      <div className="w-full md:w-[70%]  md:text-right flex flex-wrap flex-col justify-center items-center text-right p-8">
+        <div className="text-7xl md:text-[7rem] my-7 text-[#f2994a]">
+          <h1 className="font-extrabold text-right ">I came I saw I won</h1>
         </div>
-        <p className="text-base md:text-lg">
+        <p className="text-base md:text-lg ms-7">
           Adobe Illustrator is my professional Identity. I am thank full to
           Adobe and hats of to behind the scean peoples. About a decade{" "}
           <span>I am</span> Kamrul Hasan Experienched thousands of People I
