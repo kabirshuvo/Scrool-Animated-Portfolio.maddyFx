234bb592dd72a36bda096238788d32ab7a63edba
Kabir <kabirshuvo19@gmail.com>
2024-04-22 19:30:52 +0600

    Css styles added to the simple HeroSection.css

--- src/components/heros/SimpleHeroSection.tsx
+++ src/components/heros/SimpleHeroSection.tsx
@@ -1,14 +1,43 @@
-import React from "react";
+"use client";
+
+import React, { useRef, useEffect } from "react";
 import "../../../styles/simpleHeroSection.css";
 
 const SimpleHeroSection = () => {
+  const leftRef = useRef<HTMLDivElement>(null);
+
+  const handleOnMove = (e: MouseEvent | TouchEvent) => {
+    if (leftRef.current) {
+      const p =
+        ((e instanceof MouseEvent ? e.clientX : e.touches[0].clientX) /
+          window.innerWidth) *
+        100;
+
+      leftRef.current.style.width = `${p}%`;
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousemove", handleOnMove);
+    document.addEventListener("touchmove", handleOnMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleOnMove);
+      document.removeEventListener("touchmove", handleOnMove);
+    };
+  }, []);
+
   return (
-    <div>
-      <div id="left-slide" className="slide">
-        <h2 className="title">Today is going to be awesome</h2>
+    <div className="h-screen">
+      <div id="left-side" className="side" ref={leftRef}>
+        <h2 className="title">
+          Today is going to be <span className="fancyWord">awesome</span>
+        </h2>
       </div>
-      <div id="right-slide" className="slide">
-        <h2 className="title">Today is going to be wonderful</h2>
+      <div id="right-side" className="side">
+        <h2 className="title">
+          Today is going to be <span className="fancyWord"> wonderful </span>{" "}
+        </h2>
       </div>
     </div>
   );
